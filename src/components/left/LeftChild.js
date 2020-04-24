a2ad2bcf832ce8dc0f4e956d6d4a91ab49314099
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-04-24 12:02:27 -0600

    changes

--- src/components/left/LeftChild.js
+++ src/components/left/LeftChild.js
@@ -15,30 +15,38 @@ const LeftChild = (props) => {
   return (
     <div className="left-child">
       Left Child - Do stuff here
-      <input
-        type="text"
-        value={value1}
-        onChange={(e) => setValue1(e.target.value)}
-      />
-      <button onClick={submitValue1}>Submit Value 1</button>
-      <input
-        type="text"
-        value={value2}
-        onChange={(e) => setValue2(e.target.value)}
-      />
-      <button onClick={submitValue2}>Submit Value 2</button>
-      <input
-        type="text"
-        value={value3}
-        onChange={(e) => setValue3(e.target.value)}
-      />
-      <button onClick={submitValue3}>Submit Value 3</button>
-      <input
-        type="text"
-        value={value4}
-        onChange={(e) => setValue4(e.target.value)}
-      />
-      <button onClick={submitValue4}>Submit Value 4</button>
+      <div className="form-row">
+        <input
+          type="text"
+          value={value1}
+          onChange={(e) => setValue1(e.target.value)}
+        />
+        <button onClick={submitValue1}>Submit Value 1</button>
+      </div>
+      <div className="form-row">
+        <input
+          type="text"
+          value={value2}
+          onChange={(e) => setValue2(e.target.value)}
+        />
+        <button onClick={submitValue2}>Submit Value 2</button>
+      </div>
+      <div className="form-row">
+        <input
+          type="text"
+          value={value3}
+          onChange={(e) => setValue3(e.target.value)}
+        />
+        <button onClick={submitValue3}>Submit Value 3</button>
+      </div>
+      <div className="form-row">
+        <input
+          type="text"
+          value={value4}
+          onChange={(e) => setValue4(e.target.value)}
+        />
+        <button onClick={submitValue4}>Submit Value 4</button>
+      </div>
       <br />
       <br />
       <br />
